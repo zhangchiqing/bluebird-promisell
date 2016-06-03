54e68bc70a52e4d4cf052b55a904c7f723202191
zhangchiqing <zhangchiqing@gmail.com>
2016-06-03 09:25:30 -0700

    update README.md

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 var Promise = require('bluebird');
 
-// # purep :: a -> Promise a
-// .
-// . Takes any value, returns a resolved Promise with that value
-// .
-// . ```
-// . > purep(3).then(console.log)
-// . promise
-// . 3
-// . ```
+//# purep :: a -> Promise a
+//.
+//. Takes any value, returns a resolved Promise with that value
+//.
+//. ```
+//. > purep(3).then(console.log)
+//. promise
+//. 3
+//. ```
 exports.purep = function(a) {
   return Promise.resolve(a);
 };
@@ -100,13 +100,13 @@ var pipe = function() {
   };
 };
 
-// # fmapp :: (a -> b) -> Promise a -> Promise b
+//# fmapp :: (a -> b) -> Promise a -> Promise b
 //
-// . Transforms a Promise of value a into a Promise of value b
+//. Transforms a Promise of value a into a Promise of value b
 //
-// . > fmapp(function(a) { return a + 3; }, Promise.resolve(4)).then(console.log)
-// . promise
-// . 7
+//. > fmapp(function(a) { return a + 3; }, Promise.resolve(4)).then(console.log)
+//. promise
+//. 7
 exports.fmapp = function(fn) {
   return function(p) {
     return p.then(fn);
@@ -114,40 +114,40 @@ exports.fmapp = function(fn) {
 };
 
 
-// # sequencep :: Array Promise a -> Promise Array a
+//# sequencep :: Array Promise a -> Promise Array a
 //
-// . Transforms an array of Promise of value a into a Promise of array of a.
+//. Transforms an array of Promise of value a into a Promise of array of a.
 //
-// . > sequencep([Promise.resolve(3), Promise.resolve(4)]).then(console.log)
-// . promise
-// . [3, 4]
+//. > sequencep([Promise.resolve(3), Promise.resolve(4)]).then(console.log)
+//. promise
+//. [3, 4]
 exports.sequencep = function(arr) {
   return Promise.all(arr);
 };
 
-// # traversep :: (a -> Promise b) -> Array a -> Promise Array b
+//# traversep :: (a -> Promise b) -> Array a -> Promise Array b
 //
-// . Maps a function that takes a value a and returns a Promise of value b over an array of value a,
-// . then use `sequencep` to transform the array of Promise b into a Promise of array b
+//. Maps a function that takes a value a and returns a Promise of value b over an array of value a,
+//. then use `sequencep` to transform the array of Promise b into a Promise of array b
 //
-// . > traversep(function(a) { return Promise.resolve(a + 3); })(
-// .     [2, 3, 4])
-// . promise
-// . [5, 6, 7]
+//. > traversep(function(a) { return Promise.resolve(a + 3); })(
+//.     [2, 3, 4])
+//. promise
+//. [5, 6, 7]
 exports.traversep = function(fn) {
   return pipe(map(fn), exports.sequencep);
 };
 
-// # pipep :: [(a -> Promise b), (b -> Promise c), ... (m -> Promise n)] -> a -> Promise n
+//# pipep :: [(a -> Promise b), (b -> Promise c), ... (m -> Promise n)] -> a -> Promise n
 //
-// . Performs left-to-right composition of an array of Promise-returning functions.
+//. Performs left-to-right composition of an array of Promise-returning functions.
 //
-// . > pipep([
-// .     function(a) { return Promise.resolve(a + 3); },
-// .     function(b) { return Promise.resolve(b * 10); },
-// .   ])(6);
-// . promise
-// . 90
+//. > pipep([
+//.     function(a) { return Promise.resolve(a + 3); },
+//.     function(b) { return Promise.resolve(b * 10); },
+//.   ])(6);
+//. promise
+//. 90
 exports.pipep = function(pipes) {
   return function(a) {
     return fold(function(accp, fn) {
@@ -156,78 +156,78 @@ exports.pipep = function(pipes) {
   };
 };
 
-// # liftp :: (a -> b -> ... n -> x)
-//          -> Promise a -> Promise b -> ... -> Promise n
-//          -> Promise x
+//# liftp :: (a -> b -> ... n -> x)
+//         -> Promise a -> Promise b -> ... -> Promise n
+//         -> Promise x
 //
-// . Takes a function, which takes n arguments and return a value x, and n Promises,
-// . return a Promise of value x
+//. Takes a function, which takes n arguments and return a value x, and n Promises,
+//. return a Promise of value x
 //
-// . > liftp(function(a, b, c) { return (a + b) * c; })(
-// .     Promise.resolve(3),
-// .     Promise.resolve(4),
-// .     Promise.resolve(5))
-// . promise
-// . 35
+//. > liftp(function(a, b, c) { return (a + b) * c; })(
+//.     Promise.resolve(3),
+//.     Promise.resolve(4),
+//.     Promise.resolve(5))
+//. promise
+//. 35
 exports.liftp = function(fn) {
   return function() {
     return exports.fmapp(apply(fn))(exports.sequencep(toArray(arguments)));
   };
 };
 
-// alias <* firstp
-// # firstp :: Promise a -> Promise b -> Promise a
+//# firstp :: Promise a -> Promise b -> Promise a
 //
-// . Takes two Promises and return the first if both of them are resolved
+//. Takes two Promises and return the first if both of them are resolved
+//. alias <* firstp
 //
-// . > firstp(Promise.resolve(1), Promise.resolve(2))
-// . promise
-// . 1
+//. > firstp(Promise.resolve(1), Promise.resolve(2))
+//. promise
+//. 1
 //
-// . > firstp(Promise.resolve(1), Promise.reject(new Error(3)))
-// . promise
-// . Error 3
+//. > firstp(Promise.resolve(1), Promise.reject(new Error(3)))
+//. promise
+//. Error 3
 exports.firstp = exports.liftp(always);
 
-// alias *> secondp
-// # secondp :: Promise a -> Promise b -> Promise b
+//# secondp :: Promise a -> Promise b -> Promise b
 //
-// . Takes two Promises and return the second if both of them are resolved
+//. Takes two Promises and return the second if both of them are resolved
+//. alias *> secondp
 //
-// . > secondp(Promise.resolve(1), Promise.resolve(2))
-// . promise
-// . 2
+//. > secondp(Promise.resolve(1), Promise.resolve(2))
+//. promise
+//. 2
 //
-// . > secondp(Promise.resolve(1), Promise.reject(new Error(3)))
-// . promise
-// . Error 3
+//. > secondp(Promise.resolve(1), Promise.reject(new Error(3)))
+//. promise
+//. Error 3
 exports.secondp = exports.liftp(always(id));
 
-// # filterp :: (a -> Boolean) -> Array Promise a -> Promise Array a
+//# filterp :: (a -> Boolean) -> Array Promise a -> Promise Array a
 //
-// . Takes a predicate and an array of Promise a, returns a Promise of array a
-// . which satisfy the predicate.
+//. Takes a predicate and an array of Promise a, returns a Promise of array a
+//. which satisfy the predicate.
 //
-// . > filterp(function(a) { return a > 3; })([
-// .     Promise.resolve(2),
-// .     Promise.resolve(3),
-// .     Promise.resolve(4)])
-// . promise
-// . [4]
+//. > filterp(function(a) { return a > 3; })([
+//.     Promise.resolve(2),
+//.     Promise.resolve(3),
+//.     Promise.resolve(4)])
+//. promise
+//. [4]
 exports.filterp = function(fn) {
   return pipe(exports.sequencep, exports.fmapp(filter(fn)));
 };
 
-// # foldp :: (b -> a -> Promise b) -> b -> Array a -> Promise b
+//# foldp :: (b -> a -> Promise b) -> b -> Array a -> Promise b
 //
-// . Returns a Promise of value b by iterating over an array of value a, successively
-// . calling the iterator function and passing it an accumulator value of value b,
-// . and the current value from the array, and then waiting until the promise resolved,
-// . then passing the result to the next call.
+//. Returns a Promise of value b by iterating over an array of value a, successively
+//. calling the iterator function and passing it an accumulator value of value b,
+//. and the current value from the array, and then waiting until the promise resolved,
+//. then passing the result to the next call.
 //
-// . > foldp(function(b, a) { return Promise.resolve(b + a); })(1)([2, 3, 4])
-// . promise
-// . 10
+//. > foldp(function(b, a) { return Promise.resolve(b + a); })(1)([2, 3, 4])
+//. promise
+//. 10
 exports.foldp = function(fn) {
   return function(init) {
     return function(arr) {
